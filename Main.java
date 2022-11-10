79ab458a9884cfa2058351903d1ecbf5d8a7c787
nishikawa <k.nishikawa.sys22@morijyobi.ac.jp>
2022-11-10 11:19:04 +0900

    BMIを2人分追加

--- Main.java
+++ Main.java
@@ -2,16 +2,17 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner stdIn = new Scanner(System.in);
- 
+        for(int i=1;i<=2;i++){
        
-        System.out.print("身長:");
+        System.out.print(i+"人目の身長");
         double height = stdIn.nextDouble(); 
-        System.out.print("体重:");
+        System.out.print(i+"人目の体重:");
         double weight = stdIn.nextDouble(); 
         double bmi = weight / Math.pow(height, 2);
      
      
-        System.out.println("BMI値:"+(Math.floor(bmi*100)/100));
+        System.out.println(i+"人目のBMI値:"+(Math.floor(bmi*100)/100));
+	}
       }
     }
 
